fa99372da81b298be811f7c585154b02ba590d83
brianngeno <bkn.ngeno@gmail.com>
2018-08-24 17:38:13 +0300

    added a function to save new user and delete user

---
+++ run.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3.6
+from user import User
+def create_user(user_name,password):
+    '''
+    function that creates a new user
+    '''
+    new_user = User(user_name,password)
+    return new_contact
+
+
+def save_user(user)
+    '''
+    function to save new user
+    '''
+    user.save_user()
+
+
+def delete_user(user)
+    '''
+    function that delete user
+    '''
+    user.delete_user()
+
+
