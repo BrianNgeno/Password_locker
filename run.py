bba4f887d87c96784c749d2bfe304714f1b3c992
brianngeno <bkn.ngeno@gmail.com>
2018-08-24 17:52:31 +0300

    added function to copy password

--- run.py
+++ run.py
@@ -5,20 +5,23 @@ def create_user(user_name,password):
     function that creates a new user
     '''
     new_user = User(user_name,password)
-    return new_contact
+    return new_user
 
-
-def save_user(user)
+def save_user(user):
     '''
     function to save new user
     '''
     user.save_user()
 
 
-def delete_user(user)
+def delete_user(user):
     '''
     function that delete user
-    '''
+     '''
     user.delete_user()
 
-
+def copy_password(user)
+    '''
+    function that copies password
+    '''
+    user.copy_password()
